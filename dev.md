7315c52f713c8eb635712b03bb738548f7c4e541
aadityadike <adityadike123@gmail.com>
2023-12-23 13:01:21 +0530

    project initialization

---
+++ dev.md
@@ -0,0 +1,2 @@
+For Spring banner you can visit the following website: https://devops.datenkollektiv.de/banner.txt/index.html
+also use Doom as banner font
---
+++ video-uploading/src/main/java/com/streamingService/videoUploading/VideoUploadingApplication.java
@@ -0,0 +1,11 @@
+package com.streamingService.videoUploading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoUploadingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoUploadingApplication.class,args);
+    }
+}
